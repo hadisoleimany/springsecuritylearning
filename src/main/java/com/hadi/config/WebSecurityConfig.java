05002cea699658a9bfea0310abd2fe98179bc6c7
Hadi <ha.soleimany@gmail.com>
2022-01-07 22:44:12 +0400

    Test For Secure endpoints except one

--- src/main/java/com/hadi/config/WebSecurityConfig.java
+++ src/main/java/com/hadi/config/WebSecurityConfig.java
@@ -19,10 +19,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser("hadi").password("$2a$10$qLBgjIwTSWOTrsaworVXI.VD0ywaLPmXlojIFUHyBe5iKH6prAWSW").roles("admin");
     }
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-////        http.authorizeRequests().anyRequest().denyAll();
-//    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/hi").authenticated()//should log in
+                .antMatchers("/bye").authenticated()//should log in
+                .antMatchers("/").permitAll()//shouldn't log in
+                .and().formLogin().and().httpBasic();
+    }
 
     @Bean
     PasswordEncoder getPasswordEncoder(){
