d2310a0fa439d0b6368cd9a8735cb5f64deed803
Hadi <ha.soleimany@gmail.com>
2022-01-09 01:04:27 +0400

    Add role for users

--- src/main/java/com/hadi/config/WebSecurityConfig.java
+++ src/main/java/com/hadi/config/WebSecurityConfig.java
@@ -24,16 +24,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder);
-//        auth.inMemoryAuthentication().withUser("hadi").password("$2a$10$qLBgjIwTSWOTrsaworVXI.VD0ywaLPmXlojIFUHyBe5iKH6prAWSW").roles("admin");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/hi").authenticated()//should log in
-                .antMatchers("/bye").authenticated()//should log in
+                .antMatchers("/hi").hasAuthority("ADMIN")//should log in
+                .antMatchers("/bye").hasAuthority("USER")//should log in
                 .antMatchers("/").permitAll()//shouldn't log in
                 .and()
                 .formLogin().loginPage("/customLogInPage")//add custom form Login
